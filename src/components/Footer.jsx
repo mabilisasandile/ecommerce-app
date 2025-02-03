af8b28042ad6ab693ea50df56b8e0e295181c373
mabilisasandile <mabilisasandile@gmail.com>
2025-02-03 23:07:31 +0200

    cart development

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = () => {
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         
         <div>
-          <img src={assets.logo2} className="mb-5 w-32" alt="" />
+          {/* <img src={assets.logo2} className="mb-5 w-32" alt="" /> */}
+          <p className="text-xl font-medium mb-5">ABX FASHION</p>
           <p className="w-full md:w-2/3 text-gray-600">
             Lorem ipsum is simply dummy text of the printing and typesetting
             industry. Lorem ipsum is simply dummy text of the printing and
